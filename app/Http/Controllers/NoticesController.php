3d79c66350cbcb031b344c554926ee1292080164
Alex Quintana <94649578+alexquintana52@users.noreply.github.com>
2023-10-13 21:02:28 -0300

    home, blog

---
+++ app/Http/Controllers/NoticesController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NoticesController extends Controller
+{
+    public function index(){
+        return view('notices.index');
+    }
+}
---
+++ resources/views/notices/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.main')
+
+@section('title', 'Ennoia | Noticias y Novedades')
+
+@section('content')
+
+<h1>
+  Noticias
+</h1>
+
+@endsection
