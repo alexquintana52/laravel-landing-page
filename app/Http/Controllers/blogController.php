36125cc8c509ecd9406ced37f7da79ea318b100a
Alex Quintana <94649578+alexquintana52@users.noreply.github.com>
2023-12-11 19:08:52 -0300

    blog controller

--- app/Http/Controllers/blogController.php
+++ app/Http/Controllers/blogController.php
@@ -1,23 +1,48 @@
 <?php
 
+
 namespace App\Http\Controllers;
+
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\blog;
 
-class blogController extends Controller
+
+class BlogController extends Controller
 {
 
-    public function index(){
+
+    public function index(Request $request)
+    {
+        $buscarParametro = [
+            'titulo' => $request->query('titulo'),
+            'categoria_id' => $request->query('categoria_id'),
+        ];
+
+
+        $consulta = Blog::with(['categoria_blog']);
+
+
+        if($buscarParametro['titulo'] !== null){
+            $consulta -> where('titulo', 'LIKE', '%' . $buscarParametro['titulo'] . '%');
+        }
+
+
+
+
+        /** @var LengthAwarePaginator $consulta  */
+        $blogPost = $consulta->paginate(2)->withQueryString();
 
 
         return view('blog.home',[
-            'blogPost' => Blog::with('categoria_blog')->get()
+            'blogPost' => $blogPost,
         ]);
     }
 
-    public function show(int $id){
+
+    public function show(int $id)
+    {
         return view('blog.showPostBlog',[
             'blogPost' => Blog::findOrFail($id)
         ]);
